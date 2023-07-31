052d1453c6d24ea5eff732d563fcd553bb5ec51f
Jessica Carrillo <jessymamo@gmail.com>
2023-07-30 20:39:54 -0400

    Completed lab

--- main.js
+++ main.js
@@ -3,9 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.querySelectorAll(".like-glyph").forEach(element => element.addEventListener('click', handleLike));
 
+function handleLike(e) {
+  console.log(e)
+  const element = e.target
+  if (element.innerText === EMPTY_HEART){
+    mimicServerCall()
+    .then(function (message) {
+      handleSuccess(element);
+    })
+    .catch(function (errorMessage) {
+      handleError(errorMessage);
+    });
+  } else{
+    element.innerText = EMPTY_HEART
+    element.classList.remove("activated-heart");
+  }
+ 
+}
 
+function handleSuccess(element){
+  console.log("success")
+  console.log(element)
+  element.innerText = FULL_HEART
+  element.classList.add("activated-heart");
+}
 
+function handleError(errorMessage){
+ const modal = document.querySelector("#modal")
+ modal.classList.remove("hidden");
+ const modalMessage = document.querySelector("#modal-message");
+ modalMessage.innerText = errorMessage
+ setTimeout(() => {
+    modal.classList.add("hidden");
+  }, 3000);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
